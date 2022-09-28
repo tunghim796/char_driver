dbeb35a74780773f0eefc72a139fcccc64b0c994
ubuntu18 <hoangtung796@gmail.com>
2022-09-28 07:48:08 -0700

    second message

--- vchar_driver.c
+++ vchar_driver.c
@@ -95,6 +95,41 @@ int vchar_hw_read_data(vchar_dev_t *hw, int start_reg, int num_regs, char* kbuf)
 }
 
 /* ham ghi vao cac thanh ghi du lieu cua thiet bi */
+int vchar_hw_write_data(vchar_dev_t *hw, int start_reg, int num_regs, char* kbuf)
+{
+    int write_bytes = num_regs;
+    //kiem tra xem co quyen ghi du lieu khong
+    if((hw->control_regs[CONTROL_ACCESS_REG] & CTRL_WRITE_DATA_BIT) == DISABLE)
+        return -1;
+    //kiem tra xem dia chi cua kernel buffer co hop le khong
+    if(kbuf == NULL)
+        return -1;
+
+    //kiem tra xem vi tri cua cac thanh ghi can ghi co hop ly khong
+    if(start_reg > NUM_DATA_REGS)
+        return -1;
+    
+    //dieu chinh lai so luong thanh ghi du lieu can ghi (neu can thiet)
+    if (num_regs > (NUM_DATA_REGS - start_reg))
+    {
+        write_bytes = NUM_DATA_REGS - start_reg;
+        hw->status_regs[DEVICE_STATUS_REG] |= STS_DATAREGS_OVERFLOW_BIT; 
+    }
+
+    //doc du lieu tu cac thanh ghi du lieu vao kernel buffer
+    memcpy(hw->data_regs + start_reg, kbuf, write_bytes);
+
+    //cap nhat so lan ghi avo cac thanh ghi du lieu
+    hw->status_regs[WRITE_COUNT_L_REG] += 1;
+    if(hw->status_regs[WRITE_COUNT_L_REG] == 0)
+        hw->status_regs[WRITE_COUNT_H_REG] += 1;
+
+    //tra ve so byte da ghi vao cac thanh ghi du lieu
+    return write_bytes;
+    
+
+}
+
 
 /* ham doc tu cac thanh ghi trang thai cua thiet bi */
 
@@ -131,7 +166,7 @@ static ssize_t vchar_driver_read(struct file *filp, char __user *user_buf, size_
     {
         return 0;
     }
-    num_bytes = vchar_hw_read_data(vchar_drv.vchar_hw *off, kernel_buf);
+    num_bytes = vchar_hw_read_data(vchar_drv.vchar_hw, *off, len, kernel_buf);
     printk(" read %d bytes from HW\n", num_bytes);
 
     if (num_bytes < 0)
@@ -148,11 +183,32 @@ static ssize_t vchar_driver_read(struct file *filp, char __user *user_buf, size_
 
 }
 
+static ssize_t vchar_driver_write(struct file *filp, const char __user *user_buf, size_t len, loff_t *off)
+{
+    char *kernel_buf = NULL;
+    int num_bytes = 0;
+    printk("Handle write event start  from %lld, %zu bytes\n", *off, len);
+
+    kernel_buf = kzalloc(len, GFP_KERNEL);
+    if(copy_from_user(kernel_buf, user_buf, len))
+        return -EFAULT;
+    num_bytes = vchar_hw_write_data(vchar_drv.vchar_hw, *off, len, kernel_buf);
+    printk("write %d bytes to HW\n", num_bytes);
+
+    if(num_bytes < 0)
+        return -EFAULT;
+
+    *off += num_bytes;
+    return num_bytes;
+}
+
 static struct file_operations fops =
 {
     .owner      = THIS_MODULE,
     .open       = vchar_driver_open,
     .release    = vchar_driver_release,
+    .read       = vchar_driver_read,
+    .write      = vchar_driver_write,
 };
 
 
